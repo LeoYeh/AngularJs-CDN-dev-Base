b81bed3769d6bc5ca82a226c58c5b626ff79aaee
LeoYeh <homexd@gmail.com>
2015-02-20 18:18:29 +0800

    angular.js, angular-route, html5-boilerplate {{ jquery, modernizr }}

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).
+config(['$routeProvider', function($rp) {
+    // var $rp = $routeProvider;
+    $rp.otherwise({redirectTo: '/front'});
+    $rp.when("/front",function(){
+        templateUrl: 'front.html',
+        controller: 'FrontCtrl'
+    });
+}]);
